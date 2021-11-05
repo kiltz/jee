291fe65a553d6495f5a5415a7d05787a382b4668
Friedrich Kiltz <f@kiltz.de>
2021-11-05 12:38:14 +0100

    JPA mit Maven

--- 06-JPA-Features/src/test/java/de/kiltz/jpa/features/lock/OptLockTest.java
+++ 06-JPA-Features/src/test/java/de/kiltz/jpa/features/lock/OptLockTest.java
@@ -7,55 +7,56 @@ import org.hibernate.cfg.Configuration;
 
 public class OptLockTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// Einlesen der Standard-Konfig (aus der hibernate.cfg.xml)
-		Configuration conf = new Configuration();
-		conf.configure();
-		// Erzeugung der Session.
-		SessionFactory sessionFactory = conf.buildSessionFactory();
-		Session s1 = sessionFactory.openSession();
-		Session s2 = sessionFactory.openSession();
-		if (s1 == s2) {
-			System.out.println("Fehler! Die Sessions sind gleich!");
-		}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		OptLockKunde k = new OptLockKunde("1246", "Locking", "Optimist");
-		s1.beginTransaction();
-		s1.persist(k);
-		s1.getTransaction().commit();
-		System.out.println("Nach Persist: " + k.getVersion());
+        // Einlesen der Standard-Konfig (aus der hibernate.cfg.xml)
+        Configuration conf = new Configuration();
+        conf.configure();
+        // Erzeugung der Session.
+        SessionFactory sessionFactory = conf.buildSessionFactory();
+        Session s1 = sessionFactory.openSession();
+        Session s2 = sessionFactory.openSession();
+        if (s1 == s2) {
+            System.out.println("Fehler! Die Sessions sind gleich!");
+        }
 
-		OptLockKunde k2 = (OptLockKunde) s2.get(OptLockKunde.class, k.getId());
-		if (k == k2) {
-			System.out.println("Fehler! Die Kunden sind gelich!");
-		}
-		// Aus der Session holen
-		s1.evict(k);
-		s2.evict(k2);
-		System.out.println("Versionsnummern: " + k.getVersion() + "/"
-				+ k2.getVersion());
-		k.setName("Geändert");
-		k2.setVorname("Auch geändert");
-		s1.beginTransaction();
-		s1.update(k);
-		s1.getTransaction().commit();
-		System.out.println("Versionsnummer k: " + k.getVersion());
-		try {
-			s2.beginTransaction();
-			s2.update(k2);
-			System.out.println("Versionsnummer k2: " + k2.getVersion());
-			s2.getTransaction().commit();
+        OptLockKunde k = new OptLockKunde("1246", "Locking", "Optimist");
+        s1.beginTransaction();
+        s1.persist(k);
+        s1.getTransaction().commit();
+        System.out.println("Nach Persist: " + k.getVersion());
 
-		} catch (StaleObjectStateException e) {
-			System.out.println("Fehler geworfen, was richtig war! "
-					+ e.getMessage());
-		}
+        OptLockKunde k2 = (OptLockKunde) s2.get(OptLockKunde.class, k.getId());
+        if (k == k2) {
+            System.out.println("Fehler! Die Kunden sind gelich!");
+        }
+        // Aus der Session holen
+        s1.evict(k);
+        s2.evict(k2);
+        System.out.println("Versionsnummern: " + k.getVersion() + "/"
+                + k2.getVersion());
+        k.setName("Geändert");
+        k2.setVorname("Auch geändert");
+        s1.beginTransaction();
+        s1.update(k);
+        s1.getTransaction().commit();
+        System.out.println("Versionsnummer k: " + k.getVersion());
+        try {
+            s2.beginTransaction();
+            s2.update(k2);
+            System.out.println("Versionsnummer k2: " + k2.getVersion());
+            s2.getTransaction().commit();
 
-		System.out.println(k.getVorname() + " " + k.getName());
-		s1.delete(k);
-	}
+        } catch (StaleObjectStateException e) {
+            System.out.println("Fehler geworfen, was richtig war! "
+                    + e.getMessage());
+        }
+
+        System.out.println(k.getVorname() + " " + k.getName());
+        s1.delete(k);
+    }
 
 }
