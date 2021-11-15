1b29e06059ced65c9cca32add0f776a8588fabb0
kiltz <f@kiltz.de>
2021-11-15 09:43:03 +0100

    JBOSS_HOME korrigiert

--- installClientJar.bat
+++ installClientJar.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=D:/Programme/jboss-eap-7.3/bin/client/jboss-client.jar -DgroupId=org.jboss -DartifactId=jboss-client -Dversion=7.3
+mvn install:install-file -Dfile=%JBOSS_HOME%/bin/client/jboss-client.jar -DgroupId=org.jboss -DartifactId=jboss-client -Dversion=7.3
