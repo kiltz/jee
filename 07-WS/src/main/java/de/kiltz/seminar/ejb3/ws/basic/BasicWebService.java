4d375bc3b420f294ba03c5654fa725dacef40f0d
kiltz <f@kiltz.de>
2021-11-17 13:27:03 +0100

    Mit Aufgabe

--- 07-WS/src/main/java/de/kiltz/seminar/ejb3/ws/basic/BasicWebService.java
+++ 07-WS/src/main/java/de/kiltz/seminar/ejb3/ws/basic/BasicWebService.java
@@ -1,10 +1,14 @@
 package de.kiltz.seminar.ejb3.ws.basic;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface BasicWebService {
 
-	public String ping(String txt);
-	
+
+	 String ping(@WebParam(name = "txt") String txt);
+
+
 }
